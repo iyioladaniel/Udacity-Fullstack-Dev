75e955bf28ea0e3b1fc0725f2b50994284496e6c
Daniel Iyiola <iyiola_daniel@yahoo.com>
2022-08-04 15:07:19 +0100

    create table with columns using ORM

--- flask-hello-app2.py
+++ flask-hello-app2.py
@@ -2,5 +2,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+#link flask to db instance using SQLAlchemy
+db = SQLAlchemy(app)
+
+#Connect to db with flask by set configuration variable
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/dbname'
+
+#currently db is an interface for interacting with our database
+#db.Model lets us create & manipulate data models
+#db.session lets us create and manipulate database transactions
+
+#Create table called person
+class Person(db.Model):
+    #to customise the table name
+    __tablename__ = 'persons'
+    
+    #create id column
+    id = db.Column(db.Integer, primary_key=True)
+    
+    #create name column
+    name = db.Column(db.String(), nullable=False)
+
+#create table command - creates table if table doesn't exist
+db.create_all()
+
+#run application by calling path if no module imported, run app
 if __name__ == '__main__':
     app.run(host="0.0.0.0")

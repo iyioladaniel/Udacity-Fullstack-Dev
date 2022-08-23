506acef74c713541035ce53be58535875ef21537
Daniel Iyiola <iyiola_daniel@yahoo.com>
2022-08-23 07:40:36 +0100

    added a todolist table and upgraded db

--- todoapp/app.py
+++ todoapp/app.py
@@ -24,9 +24,19 @@ class Todo(db.Model):
     title = db.Column(db.String(), nullable=False)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean,nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
-        return f'<Todo {self.id} {self.title} {self.description}'
+        return f'<Todo {self.id} {self.title} {self.description}>'
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
+    def __repr__(self):
+        return f'<TodolList {self.id} {self.name}>'
 
 db.create_all()
 
